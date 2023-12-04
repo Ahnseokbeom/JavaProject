d9a4b1da6172b1b49543a0f6c090c69a5d6b3014
seobkeom <ajm153123@naver.com>
2023-12-04 15:35:23 +0900

    Feat : Game Level Update

--- ProjectPractice/src/MineSweeperGame/MineSweeperGame.java
+++ ProjectPractice/src/MineSweeperGame/MineSweeperGame.java
@@ -4,18 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 public class MineSweeperGame extends JFrame {
-    private int numRows = 10;
-    private int numColumns = 10;
-    private int numMines = 20;
+    private int numRows;
+    private int numColumns;
+    private int numMines;
     private JButton[][] grid;
     private boolean[][] mineLocations;
     private boolean[][] revealed;
-    private boolean gameOver = false;
+    private boolean gameOver;
 
-    public MineSweeperGame() {
+    public MineSweeperGame(int numRows, int numColumns, int numMines) {
+        this.numRows = numRows;
+        this.numColumns = numColumns;
+        this.numMines = numMines;
+
         grid = new JButton[numRows][numColumns];
         mineLocations = new boolean[numRows][numColumns];
         revealed = new boolean[numRows][numColumns];
@@ -26,7 +34,7 @@ public class MineSweeperGame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("지뢰찾기");
         pack();
-        
+
         setLocationRelativeTo(null);
     }
 
@@ -75,8 +83,12 @@ public class MineSweeperGame extends JFrame {
         if (mineLocations[row][col]) {
             gameOver = true;
             showMines();
-            JOptionPane.showMessageDialog(this, "지뢰를 밟았습니다. 게임 종료!");
-            askForRestart();
+            int choice = JOptionPane.showConfirmDialog(this, "지뢰를 밟았습니다. 게임 종료! 다시 시작하시겠습니까?", "게임 종료", JOptionPane.YES_NO_OPTION);
+            if (choice == JOptionPane.YES_OPTION) {
+                restartGame();
+            } else {
+                System.exit(0);
+            }
         } else {
             int adjacentMines = countAdjacentMines(row, col);
 
@@ -88,8 +100,13 @@ public class MineSweeperGame extends JFrame {
 
             if (checkGameWin()) {
                 gameOver = true;
-                JOptionPane.showMessageDialog(this, "축하합니다! 모든 지뢰를 찾았습니다. 게임 종료!");
-                askForRestart();
+                showMines();
+                int choice = JOptionPane.showConfirmDialog(this, "축하합니다! 모든 지뢰를 찾았습니다. 게임 종료! 다시 시작하시겠습니까?", "게임 종료", JOptionPane.YES_NO_OPTION);
+                if (choice == JOptionPane.YES_OPTION) {
+                    restartGame();
+                } else {
+                    System.exit(0);
+                }
             }
         }
     }
@@ -141,15 +158,6 @@ public class MineSweeperGame extends JFrame {
         }
     }
 
-    private void askForRestart() {
-        int choice = JOptionPane.showConfirmDialog(this, "게임을 다시 시작하시겠습니까?", "게임 재시작", JOptionPane.YES_NO_OPTION);
-        if (choice == JOptionPane.YES_OPTION) {
-            restartGame();
-        } else {
-            System.exit(0);
-        }
-    }
-
     private void restartGame() {
         // Reset all game variables
         for (int row = 0; row < numRows; row++) {
@@ -163,6 +171,7 @@ public class MineSweeperGame extends JFrame {
         placeMines();
         gameOver = false;
     }
+
     private boolean checkGameWin() {
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numColumns; col++) {
@@ -173,13 +182,46 @@ public class MineSweeperGame extends JFrame {
         }
         return true; // 모든 빈 칸이 열렸으면 게임 승리
     }
+    
+    private static MineSweeperGame initializeGame() {
+        String[] options = {"초급", "중급", "고급"};
+        int choice = JOptionPane.showOptionDialog(
+                null,
+                "난이도를 선택하세요",
+                "난이도 선택",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        MineSweeperGame game;
+
+        switch (choice) {
+            case 0: // 초급
+                game = new MineSweeperGame(9, 9, 10);
+                break;
+            case 1: // 중급
+                game = new MineSweeperGame(16, 16, 40);
+                break;
+            case 2: // 고급
+                game = new MineSweeperGame(16, 30, 99);
+                break;
+            default:
+                throw new IllegalStateException("예상치 못한 값: " + choice);
+        }
+
+        return game;
+    }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MineSweeperGame game = new MineSweeperGame();
-            game.setVisible(true);
+    	SwingUtilities.invokeLater(() -> {	
+            try {
+                MineSweeperGame game = initializeGame();
+                game.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
     }
 }
-
-
